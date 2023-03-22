49d89ed73fabe52cfc744c61e9d8ed80a1426888
Ensar <ensarhamzic01@gmail.com>
2023-03-23 00:00:36 +0100

    test

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from "styled-components"
+import { Text, TextInput } from "react-native-paper"
+import { Image, View } from "react-native"
+
+const LoginView = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const LogoImage = styled.Image`
+  height: 130px;
+  resize-mode: contain;
+`
+
+const SignText = styled.Text`
+  margin-top: 20px;
+  font-size: 15px;
+  font-weight: bold;
+`
+
+const FormView = styled.View`
+  margin-top: 10px;
+  width: 80%;
+`
+
+const PasswordTextInput = styled(TextInput)`
+  margin-top: 10px;
+`
+
+const FooterView = styled.View`
+  margin-top: 10px;
+`
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <LoginView>
+      <View>
+        <LogoImage source={require("./assets/logo-full.png")} />
+      </View>
+      <View>
+        <SignText>Sign in to your account</SignText>
+      </View>
+      <FormView>
+        <TextInput label="Email" />
+        <PasswordTextInput label="Password" />
+      </FormView>
+      <FooterView>
+        <Text>Don't have an account? Sign Up</Text>
+      </FooterView>
+    </LoginView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

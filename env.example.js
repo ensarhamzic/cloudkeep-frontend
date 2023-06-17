4d3e5e91af03f519f3657fa5b1c701898e650adc
Ensar <ensarhamzic01@gmail.com>
2023-06-17 22:11:36 +0200

    updated

--- env.example.js
+++ env.example.js
@@ -1,7 +1,11 @@
 export const API_URL = ""
+
 export const FIREBASE_API_KEY = ""
 export const FIREBASE_AUTH_DOMAIN = ""
 export const FIREBASE_PROJECT_ID = ""
 export const FIREBASE_STORAGE_BUCKET = ""
 export const FIREBASE_MESSAGING_SENDER_ID = ""
 export const FIREBASE_APP_ID = ""
+
+export const IOS_GOOGLE_CLIENT_ID = ""
+export const ANDROID_GOOGLE_CLIENT_ID = ""
